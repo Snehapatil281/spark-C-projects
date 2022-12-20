ba4544069cd187dc087fccf1d712bc950185dfc4
Snehapatil281 <121081968+Snehapatil281@users.noreply.github.com>
2022-12-20 12:05:40 +0530

    Update spark.c

--- spark.c
+++ spark.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
+void add();
 main()
 {
 print("\n welcome");
+  printf("\n spark institue");
+  add();
 }
+void add()
+{
+  int a,b,c;
+  printf("\n enter a&b");
+  scanf("%d%d,&a,&b);
+        c=a+b;
+        printf("\n add=%d",c);
+        
+        }
